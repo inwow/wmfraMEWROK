7ebd6836e362cd7c2ceb72332e0c5e0494329ab1
inwow <105809098+inwow@users.noreply.github.com>
2020-03-16 20:36:07 +0000

    Bump next snapshot (#6256)

--- CODE/MengerHaantjesUnweighted.py
+++ CODE/MengerHaantjesUnweighted.py
@@ -4,4 +4,4 @@ Program to compute Menger and Haantjes curvature for edges in unweighted and
 undirected network.
 Written by: Areejit Samal
 Reference:
-(1) A. Samal, 
+(1) A. Samal, H.K. Pharasi, S. J.
