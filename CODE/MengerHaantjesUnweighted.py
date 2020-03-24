6122888e6a0b3479c96c0106eb02379df4144595
inwow <105809098+inwow@users.noreply.github.com>
2020-03-24 12:08:27 +0000

    Updated changes

--- CODE/MengerHaantjesUnweighted.py
+++ CODE/MengerHaantjesUnweighted.py
@@ -4,4 +4,4 @@ Program to compute Menger and Haantjes curvature for edges in unweighted and
 undirected network.
 Written by: Areejit Samal
 Reference:
-(1) A. Samal, H.K. Pharasi, S. J. Ramaia, H. Kannan, E. Saucan, J. Jost & A. Chakraborti, Network geometry and market instability, R
+(1) A. Samal, H.K. Pharasi, S. J. Ramaia, H. Kannan, E. Saucan, J. Jost & A. Chakraborti, Network geometry and market instability, R. Soc. Open 
