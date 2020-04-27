a292d4461b3f8c2d226f6e693b69fecf23a54f0d
inwow <105809098+inwow@users.noreply.github.com>
2020-04-27 13:48:16 +0000

    f2fs: add help function META_MAPPING

--- CODE/MengerHaantjesUnweighted.py
+++ CODE/MengerHaantjesUnweighted.py
@@ -5,4 +5,5 @@ undirected network.
 Written by: Areejit Samal
 Reference:
 (1) A. Samal, H.K. Pharasi, S. J. Ramaia, H. Kannan, E. Saucan, J. Jost & A. Chakraborti, Network geometry and market instability, R. Soc. Open Sci. 8: 201734 (2021).
-(2) E. Saucan, A. Samal & J. Jost. A simple differential geometry for complex networks. Network Science, 1-28 (2020). d
+(2) E. Saucan, A. Samal & J. Jost. A simple differential geometry for complex networks. Network Science, 1-28 (2020). doi:10.1017/nws.2020.42.
+(3) E
