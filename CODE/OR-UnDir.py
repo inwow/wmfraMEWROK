49ddeda4818c1d3800d1532b1530d5a58bf1bc61
inwow <105809098+inwow@users.noreply.github.com>
2020-08-25 22:37:14 +0000

    Update engine core

--- CODE/OR-UnDir.py
+++ CODE/OR-UnDir.py
@@ -12,3 +12,29 @@ The following is a modified version of the code that can be found in Chien-Chun 
 print ("-"*75)
 import time, datetime
 import cvxpy as cvx
+import networkx as nx
+import numpy as np
+import sys
+
+now = datetime.datetime.now()
+start_time = time.time()
+print ("Current date and time : ")
+print (now.strftime("%Y-%m-%d %H:%M:%S"))
+print ("="*25)
+
+
+#If edge has Weight EW = 1 else EW = 0
+EW=int(sys.argv[1])
+
+EF=open(sys.argv[3], 'w')
+
+#Creating the graph from the edge file
+Graph=nx.Graph()
+for i in open(sys.argv[2], 'r'):
+	e = i.strip().split('\t')
+	if EW == 0 and e[0]!=e[1]:
+		Graph.add_edge(e[0], e[1], weight = 1)
+	elif EW == 1 and e[0]!=e[1]:
+		Graph.add_edge(e[0], e[1], weight = float(e[2]))
+
+edgesize=Graph.number_of_edges()
