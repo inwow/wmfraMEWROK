46a03425c584dcc2f616c4f54aa6b126556753b9
inwow <105809098+inwow@users.noreply.github.com>
2020-09-14 05:48:19 +0000

    improve the readme

--- CODE/comm_eff.py
+++ CODE/comm_eff.py
@@ -1,3 +1,3 @@
 # Python code for calculating communication efficiency of a network.
 # The reference articles for the computed measure:
-# Latora, V., and Marchiori, M. (2001). E
+# Latora, V., and Marchiori, M. (2001). Efficient behavior 
