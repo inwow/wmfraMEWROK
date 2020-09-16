0066fb070fa1e921fffe916d454ed80d8496dd82
inwow <105809098+inwow@users.noreply.github.com>
2020-09-16 14:49:11 +0000

    Update team.md

--- CODE/comm_eff.py
+++ CODE/comm_eff.py
@@ -1,3 +1,3 @@
 # Python code for calculating communication efficiency of a network.
 # The reference articles for the computed measure:
-# Latora, V., and Marchiori, M. (2001). Efficient behavior of small-world networks. Physical Revi
+# Latora, V., and Marchiori, M. (2001). Efficient behavior of small-world networks. Physical Review Letters 8
