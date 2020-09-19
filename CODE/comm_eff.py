0433a6868be6e524723ad816e5b2fa24375e2a7e
inwow <105809098+inwow@users.noreply.github.com>
2020-09-19 13:18:22 +0000

    update beers

--- CODE/comm_eff.py
+++ CODE/comm_eff.py
@@ -2,4 +2,5 @@
 # The reference articles for the computed measure:
 # Latora, V., and Marchiori, M. (2001). Efficient behavior of small-world networks. Physical Review Letters 87.
 # Latora, V., and Marchiori, M. (2003). Economic small-world behavior in weighted networks. Eur Phys J B 32, 249-263.
-# Inpu
+# Input 1: Node_file
+# 
