71960cdb25c98bdb0884e7ecb7085fcc039abfa9
inwow <105809098+inwow@users.noreply.github.com>
2020-09-19 11:44:57 +0000

    Create bot.js

--- CODE/comm_eff.py
+++ CODE/comm_eff.py
@@ -1,4 +1,5 @@
 # Python code for calculating communication efficiency of a network.
 # The reference articles for the computed measure:
 # Latora, V., and Marchiori, M. (2001). Efficient behavior of small-world networks. Physical Review Letters 87.
-# Latora, V., and Marchiori, M. (2003). Economic small-world behavior in weighted networks. Eur Phys J B 
+# Latora, V., and Marchiori, M. (2003). Economic small-world behavior in weighted networks. Eur Phys J B 32, 249-263.
+# Inpu
