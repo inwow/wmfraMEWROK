78810d555fd67a761e08b16eb724207db9973acc
inwow <105809098+inwow@users.noreply.github.com>
2020-11-08 09:14:54 +0000

    astyle

--- CODE/comm_eff.py
+++ CODE/comm_eff.py
@@ -12,4 +12,5 @@ If you are using this code, kindly cite the following articles:
 =================================================================================================
 """
 
-import i
+import igraph as ig
+import sys
