16a7bd397781cac440aed71b2416282a79798342
inwow <105809098+inwow@users.noreply.github.com>
2020-11-17 22:38:33 +0000

    cmake cleanup

--- CODE/comm_eff.py
+++ CODE/comm_eff.py
@@ -22,4 +22,4 @@ infile=sys.argv[2]
 G=ig.Graph.Read_Ncol(infile,weights=True,directed=False,names=True)
 
 N=G.vcount()
-nodes=[i.strip().split()[0] for i in open(nodefile) if isinstance(int(i.st
+nodes=[i.strip().split()[0] for i in open(nodefile) if isinstance(int(i.strip().split()[0]),int)
