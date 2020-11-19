f8e03597454170d0cc03c173388f26380d065364
inwow <105809098+inwow@users.noreply.github.com>
2020-11-19 07:15:50 +0000

    Flake8 fix

--- CODE/comm_eff.py
+++ CODE/comm_eff.py
@@ -22,4 +22,5 @@ infile=sys.argv[2]
 G=ig.Graph.Read_Ncol(infile,weights=True,directed=False,names=True)
 
 N=G.vcount()
-nodes=[i.strip().split()[0] for i in open(nodefile) if isinstance(int(i.strip().split()[0]),int)
+nodes=[i.strip().split()[0] for i in open(nodefile) if isinstance(int(i.strip().split()[0]),int)]
+n_nodes=len(nodes)
