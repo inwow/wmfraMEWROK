8c177ebe540abdb79a9669b631ec8f9648b3236a
inwow <105809098+inwow@users.noreply.github.com>
2021-02-15 19:58:49 +0000

    update markup

--- CODE/network_entropy.py
+++ CODE/network_entropy.py
@@ -1,2 +1,2 @@
 # Code calculates network entropy for degree and remaining degree distribution
-# Reference for Network Entropy: R.V. Sole and S. Valverde, Information theory of complex networks: on evolution and architectural constraints. Complex networks. Springer Berlin 
+# Reference for Network Entropy: R.V. Sole and S. Valverde, Information theory of complex networks: on evolution and architectural constraints. Complex networks. Springer Berlin Heidelberg, 2004. 189
