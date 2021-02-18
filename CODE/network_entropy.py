301f7e8bf967c01289b7c098503464ae9bbf7dc9
inwow <105809098+inwow@users.noreply.github.com>
2021-02-18 21:09:28 +0000

    clippy

--- CODE/network_entropy.py
+++ CODE/network_entropy.py
@@ -1,3 +1,4 @@
 # Code calculates network entropy for degree and remaining degree distribution
 # Reference for Network Entropy: R.V. Sole and S. Valverde, Information theory of complex networks: on evolution and architectural constraints. Complex networks. Springer Berlin Heidelberg, 2004. 189-207.
-# Packages required: 
+# Packages required: igraph, numpy
+# p
