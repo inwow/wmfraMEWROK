5da534af333a41061c648c4362b82aab0b5c1b02
inwow <105809098+inwow@users.noreply.github.com>
2021-02-24 23:01:24 +0000

    Update sbt to 1.5.4

--- CODE/network_entropy.py
+++ CODE/network_entropy.py
@@ -1,4 +1,5 @@
 # Code calculates network entropy for degree and remaining degree distribution
 # Reference for Network Entropy: R.V. Sole and S. Valverde, Information theory of complex networks: on evolution and architectural constraints. Complex networks. Springer Berlin Heidelberg, 2004. 189-207.
 # Packages required: igraph, numpy
-# python network_entropy.py inputfile_folder_path 1/0 output
+# python network_entropy.py inputfile_folder_path 1/0 outputfile
+# 1 -- weighted
