f80d2899b11962507a182ea9cc636600b9f37269
inwow <105809098+inwow@users.noreply.github.com>
2021-04-12 05:59:32 +0000

    new recipe

--- CODE/network_entropy.py
+++ CODE/network_entropy.py
@@ -12,4 +12,5 @@ If you are using this code, kindly cite the following articles:
 =================================================================================================
 """
 
-import igraph as
+import igraph as ig
+import n
