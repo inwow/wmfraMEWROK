20166f29957970c9aefd4ca26708987a31205684
inwow <105809098+inwow@users.noreply.github.com>
2021-04-25 13:32:36 +0000

    minor bug

--- CODE/network_entropy.py
+++ CODE/network_entropy.py
@@ -23,4 +23,5 @@ infile=sys.argv[1]
 weight=bool(sys.argv[2]) #input 1 if weighted; 0 if unweighted
 outfile=sys.argv[3]
 
-#function input: dictionary of degree/remaining degree d
+#function input: dictionary of degree/remaining degree distribution
+def entrop
